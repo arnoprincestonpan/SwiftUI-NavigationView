adb8545ec9e45f9f4eaaa42327bfdad9514602f0
ARNO PAN <apan11@my.bcit.ca>
2023-07-07 02:00:01 -0700

    added buttons and simple second view

--- SwiftUI-NavigationView/ContentView.swift
+++ SwiftUI-NavigationView/ContentView.swift
@@ -9,18 +9,75 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [.blue, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .ignoresSafeArea(.all)
+                VStack{
+                    Image(systemName: "house")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 208,
+                               height: 208,
+                               alignment: .center)
+                        .padding()
+                    
+                    Text("Home Screen")
+                        .font(.system(size: 32, weight: .light, design: .default))
+                    
+                    NavigationLink(
+                        destination: SecondScreen(),
+                        label: {
+                            ContinueButton()
+                        }
+                    )
+                }
+            }
+            .navigationTitle("Home")
         }
-        .padding()
+    }
+}
+
+struct SecondScreen: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.blue, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea(.all)
+            VStack{
+                Image(systemName: "bell")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 208,
+                           height: 208,
+                           alignment: .center)
+                    .padding()
+                
+                Text("Notification")
+                    .font(.system(size: 32, weight: .light, design: .default))
+                
+                NavigationLink(
+                    destination: SecondScreen(),
+                    label: {
+                        ContinueButton()
+                    }
+                )
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ContinueButton: View {
+    var body: some View {
+        Text("Continue")
+            .frame(width: 204, height: 48, alignment: .center)
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .cornerRadius(8)
     }
 }
