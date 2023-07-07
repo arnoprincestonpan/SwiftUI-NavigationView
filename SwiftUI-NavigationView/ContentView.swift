c3825a97b2d90740757251382159c20126ab6220
ARNO PAN <apan11@my.bcit.ca>
2023-07-07 02:13:13 -0700

    modulize views

--- SwiftUI-NavigationView/ContentView.swift
+++ SwiftUI-NavigationView/ContentView.swift
@@ -10,30 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            ZStack {
-                LinearGradient(colors: [.blue, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-                    .ignoresSafeArea(.all)
-                VStack{
-                    Image(systemName: "house")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 208,
-                               height: 208,
-                               alignment: .center)
-                        .padding()
-                    
-                    Text("Home Screen")
-                        .font(.system(size: 32, weight: .light, design: .default))
-                    
-                    NavigationLink(
-                        destination: SecondScreen(),
-                        label: {
-                            ContinueButton()
-                        }
-                    )
-                }
-            }
-            .navigationTitle("Home")
+            HomeScreen()
         }
     }
 }
@@ -56,12 +33,12 @@ struct SecondScreen: View {
                     .font(.system(size: 32, weight: .light, design: .default))
                 
                 NavigationLink(
-                    destination: SecondScreen(),
+                    destination: HomeScreen(),
                     label: {
-                        ContinueButton()
+                        ContinueButton(btnText: "Home", btnBackgroundColor: .blue, btnFontColor: .white)
                     }
                 )
-            }
+            }.navigationTitle("Notifications")
         }
     }
 }
@@ -73,11 +50,45 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct ContinueButton: View {
+    
+    var btnText: String
+    var btnBackgroundColor: Color
+    var btnFontColor: Color
+    
     var body: some View {
-        Text("Continue")
+        Text(btnText)
             .frame(width: 204, height: 48, alignment: .center)
-            .background(Color.blue)
-            .foregroundColor(Color.white)
+            .background(btnBackgroundColor)
+            .foregroundColor(btnFontColor)
             .cornerRadius(8)
+    }
+}
+
+struct HomeScreen: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.blue, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea(.all)
+            VStack{
+                Image(systemName: "house")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 208,
+                           height: 208,
+                           alignment: .center)
+                    .padding()
+                
+                Text("Home Screen")
+                    .font(.system(size: 32, weight: .light, design: .default))
+                
+                NavigationLink(
+                    destination: SecondScreen(),
+                    label: {
+                        ContinueButton(btnText: "Notification", btnBackgroundColor: .blue, btnFontColor: .white)
+                    }
+                )
+            }
+            .navigationTitle("Home")
+        }
     }
 }
