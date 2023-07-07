b9d38a3797eb5458d330ec2f0492c7aeb99583e7
ARNO PAN <apan11@my.bcit.ca>
2023-07-07 01:04:40 -0700

    Initial Commit

---
+++ SwiftUI-NavigationView/SwiftUI_NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationViewApp.swift
+//  SwiftUI-NavigationView
+//
+//  Created by Arno Princeston pan on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
